3c4454d3607b316b04ec59ee166dddd46aeaa23c
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-05-04 21:34:26 -0400

    feat: extend worker pool project

--- worker-pool.go
+++ worker-pool.go
@@ -58,6 +58,7 @@ type WorkerPool struct {
 type Stats struct {
 	TasksExecuted      uint64
 	TotalExecutionTime time.Duration
+	TaskFailures       uint64
 }
 
 // NewWorkerPool returns a new worker pool
@@ -70,6 +71,10 @@ func NewWorkerPool(cfg WorkerPoolConfig) *WorkerPool {
 	}
 	wp.setWorkerCount(cfg.NumWorkers)
 	go wp.monitor()
+	for i := 0; i < cfg.NumWorkers; i++ {
+		wp.wg.Add(1)
+		go wp.worker()
+	}
 	return wp
 }
 
@@ -84,7 +89,89 @@ func (wp *WorkerPool) Stop() {
 	wp.wg.Wait()
 }
 
-// generateTasks generates tasks for demonstration purposes
+type priorityQueue struct {
+	tasks []*Task
+	mu    sync.Mutex
+	cond  *sync.Cond
+}
+
+func newPriorityQueue(size int) *priorityQueue {
+	pq := &priorityQueue{
+		tasks: make([]*Task, 0, size),
+	}
+	pq.cond = sync.NewCond(&pq.mu)
+	return pq
+}
+
+func (pq *priorityQueue) push(task *Task) {
+	pq.mu.Lock()
+	defer pq.mu.Unlock()
+	pq.tasks = append(pq.tasks, task)
+	pq.cond.Signal()
+}
+
+func (pq *priorityQueue) pop() *Task {
+	pq.mu.Lock()
+	defer pq.mu.Unlock()
+	for len(pq.tasks) == 0 {
+		pq.cond.Wait()
+	}
+	task := pq.tasks[0]
+	pq.tasks = pq.tasks[1:]
+	return task
+}
+
+func (wp *WorkerPool) worker() {
+	defer wp.wg.Done()
+	for {
+		select {
+		case <-wp.stopChan:
+			return
+		default:
+			task := wp.taskQueue.pop()
+			wp.executeTask(task)
+		}
+	}
+}
+
+func (wp *WorkerPool) executeTask(task *Task) {
+	startTime := time.Now()
+	ctx, cancel := context.WithTimeout(context.Background(), task.timeout)
+	defer cancel()
+	err := task.fn(ctx)
+	executionTime := time.Since(startTime)
+	atomic.AddUint64(&wp.stats.TasksExecuted, 1)
+	atomic.AddInt64(&wp.stats.TotalExecutionTime, int64(executionTime))
+	if err != nil {
+		atomic.AddUint64(&wp.stats.TaskFailures, 1)
+		if task.retryCount > 0 {
+			task.retryCount--
+			time.Sleep(wp.retryDelay)
+			wp.taskQueue.push(task)
+		}
+	}
+}
+
+func (wp *WorkerPool) monitor() {
+	ticker := time.NewTicker(wp.monitoringInterval)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-wp.stopChan:
+			return
+		case <-ticker.C:
+			log.Printf("Tasks executed: %d, Total execution time: %s, Task failures: %d",
+				atomic.LoadUint64(&wp.stats.TasksExecuted),
+				time.Duration(atomic.LoadInt64(&wp.stats.TotalExecutionTime)),
+				atomic.LoadUint64(&wp.stats.TaskFailures))
+		}
+	}
+}
+
+func (wp *WorkerPool) setWorkerCount(count int) {
+	atomic.StoreInt32(&wp.workerCount, int32(count))
+}
+
 func (wp *WorkerPool) generateTasks() {
 	taskID := 0
 	for {
@@ -92,7 +179,7 @@ func (wp *WorkerPool) generateTasks() {
 		case <-wp.stopChan:
 			return
 		default:
-			task := Task{
+			task := &Task{
 				id:      taskID,
 				timeout: 2 * time.Second,
 				priority: Priority(rand.Intn(3)),
